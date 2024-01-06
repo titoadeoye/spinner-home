24d3e8cab31b5d121a3747e41449bfbd7c03682b
ty-codes <titoadeoye00@gmail.com>
2024-01-06 09:27:42 -0800

    chore: add remixicon and react-router deps

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "home/Header";
+import Footer from "home/Footer";
+// import PDPContent from "pdp/PDPContent";
+import HomeContent from "home/HomeContent";
+// import CartContent from "cart/CartContent";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <Router>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      <Switch>
+        <Route exact path="/" component={HomeContent} />
+        {/* <Route path="/product/:id" component={PDPContent} /> */}
+      </Switch>
+    </div>
+    <Footer />
   </div>
+</Router>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));

c3d07699ce4ad5b09b0f85b5f1ee2912a098367d
ty-codes <titoadeoye00@gmail.com>
2024-01-06 15:55:06 -0800

    fix: fix breaking change bug by replacing with semantic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 
 import "./index.scss";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "home/Header";
 import Footer from "home/Footer";
 // import PDPContent from "pdp/PDPContent";
 import HomeContent from "home/HomeContent";
 // import CartContent from "cart/CartContent";
 
+const root = createRoot(document.getElementById("app"));
+
 const App = () => (
   <Router>
-  <div className="text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">
-      <Switch>
-        <Route exact path="/" component={HomeContent} />
-        {/* <Route path="/product/:id" component={PDPContent} /> */}
-      </Switch>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        <Routes>
+          <Route exact path="/" component={HomeContent} />
+          {/* <Route path="/product/:id" component={PDPContent} /> */}
+        </Routes>
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-</Router>
+  </Router>
 );
 
-ReactDOM.render(<App />, document.getElementById("app"));
+root.render(<App />);
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
 import MiniCart from "cart/MiniCart";
 import Login from "cart/Login";
 
 export default function Header() {
   return (
     <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
-      <div className="flex">
-        <div className="flex-grow flex">
-          <Link to="/">Fidget Spinner World</Link>
-          <div className="mx-5">|</div>
-          <Link id="cart" to="/cart">
-            Cart
-          </Link>
-        </div>
-        <div className="flex-end relative">
-          <MiniCart />
-          <Login />
+      <div className="p-5 bg-blue-500 text-white text-3xl font-bold">
+        <div className="flex">
+          <div className="flex-grow flex">
+            <a to="/">Fidget Spinner World</a>
+            <div className="mx-5">|</div>
+            <a id="cart" href="/cart">
+              Cart
+            </a>
+          </div>
+          <div className="flex-end relative">
+            <MiniCart />
+            <Login />
+          </div>
         </div>
       </div>
     </div>
